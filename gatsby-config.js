1915763a73ef3a030fd4fb0e03f246d6522cb03c
andré <andredds12@gmail.com>
2020-07-12 18:16:03 -0300

    its time to celebrate

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `pages`,
+        name: `data`,
         path: `${__dirname}/src/pages`,
       },
     },
--- src/pages/2020-07-12-first-post.md
+++ src/pages/2020-07-12-first-post.md
@@ -1,7 +1,7 @@
 ---
 path: "/hello-world"
 externalurl: "/hello-world"
-date: "2019-07-12"
+date: "2020-07-12"
 title: "My First Post"
 type: "BLOG"
 ---
